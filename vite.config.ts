09938813a756cb135e433eeaa7f41b4e94328eca
christian lind <christianlind99@gmail.com>
2025-05-27 15:58:45 +0200

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import eslintPlugin from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-/**
- * @see https://vitejs.dev/config/
- */
 export default defineConfig({
+  base: '/',
   plugins: [react(), eslintPlugin()],
   resolve: {
     alias: {
